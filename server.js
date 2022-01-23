2bc15584914b01b36addf615d43db1efc6ed5d42
Henok Mekonnen <henatan99@yahoo.com>
2022-01-23 18:03:01 +0300

    Add start script in the package.json to use nodemon

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get('/', (req, res) => {
     res.send('Server at http://localhost:5000');
 });
 
+const port = process.env.PORT || 5000;
 app.listen(5000, () => {
-    console.log('Server at http://localhost:5000');
-})
+    console.log(`Server at http://localhost:${port}`);
+});
