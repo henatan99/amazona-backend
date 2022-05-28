f8af680c58de87ec664f6213fd39f5d965e2b978
Henok Mekonnen <henatan99@yahoo.com>
2022-05-28 00:57:05 -0400

    Add product model, router and seed data

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import data from './data.js';
+// import data from './data.js';
+import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 
 const db = mongoose.connection;
@@ -18,21 +19,8 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
 .then(() => console.log("Database connected!"))
 .catch(err => console.log(err));
 
-
-app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find(x => x.id === req.params.id);
-    if(product){
-        res.send(product);
-    } else {
-        res.status(404).send({ message: 'Product not Found' });
-    }
-})
-
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
-});
-
 app.use('/api/users', userRouter)
+app.use('/api/products', productRouter)
 
 app.get('/', (req, res) => {
     res.send('Server at http://localhost:5000');
