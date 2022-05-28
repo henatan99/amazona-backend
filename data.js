87782cf51a4175520914da77934f707799e8e277
Henok Mekonnen <henatan99@yahoo.com>
2022-05-28 01:01:46 -0400

    Remove duplicate name from products list in seed data

--- data.js
+++ data.js
@@ -17,7 +17,6 @@ const data = {
     ],
     products: [
       {
-        id: '1',
         name: 'Nike Slim Shirt',
         category: 'Shirts',
         image: '/images/p1.jpg',
@@ -29,7 +28,6 @@ const data = {
         countInStock: 10,
       },
       {
-        id: '2',
         name: 'Adidas Fit Shirt',
         category: 'Shirts',
         image: '/images/p2.jpg',
@@ -41,7 +39,6 @@ const data = {
         countInStock: 17,
       },
       {
-        id: '3',
         name: 'Lacoste Shirt',
         category: 'Shirts',
         image: '/images/p3.jpg',
@@ -53,7 +50,6 @@ const data = {
         countInStock: 0,
       },
       {
-        id: '4',
         name: 'Puma Shirt',
         category: 'Shirts',
         image: '/images/p4.jpg',
@@ -63,30 +59,6 @@ const data = {
         numReviews: 10,
         description: 'high quality product',
         countInStock: 5,
-      },
-      {
-        id: '5',
-        name: 'Nike Slim Shirt',
-        category: 'Shirts',
-        image: '/images/p5.jpg',
-        price: 150,
-        brand: 'Nike',
-        rating: 3.5,
-        numReviews: 10,
-        description: 'high quality product',
-        countInStock: 7,
-      },
-      {
-        id: '6',
-        name: 'Nike Slim Shirt',
-        category: 'Shirts',
-        image: '/images/p6.jpg',
-        price: 110,
-        brand: 'Nike',
-        rating: 2,
-        numReviews: 10,
-        description: 'high quality product',
-        countInStock: 0,
       },
     ],
   };
